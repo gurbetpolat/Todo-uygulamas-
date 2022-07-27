a7307430d903f9a0f901e8beaf9a457fda56d46b
gurbetpolat <p.gurbet.p@gmail.com>
2022-07-27 15:16:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,105 @@
+var items = ["Kahvaltı ", "Spor", "Kitap Okuma", "Bisiklete Binme", "Öğle Yemeği", "Yürüyüş", "Akşam Yemeği"];
+
+var list = document.querySelector("#myList");
+
+
+
+items.forEach(function (item) {
+    CreateItem(item);
+
+});
+
+
+// var close=document.getElementsByClassName("close");
+
+// for (var i=0; i<close.length; i++){
+//     span.onclick=function(){
+//         var li=this.parentElement;
+//         li.style.display="none";
+//     }
+// }
+
+
+
+list.addEventListener("click", function (item) {
+
+
+
+    if (item.target.tagName = "LI") {
+
+        item.target.classList.toggle("checked");
+
+        ToggleDeleteButton();
+    }
+
+
+});
+
+
+function ToggleDeleteButton() {
+    var checkList = document.querySelectorAll(".list-group-item.checked");
+
+    if (checkList.length > 0) {
+        document.querySelector("#deleteAll").classList.remove("d-none");
+
+    }
+    else {
+        document.querySelector("#deleteAll").classList.add("d-none");
+
+    }
+};
+
+document.querySelector("#deleteAll").onclick=function(){
+
+    var elements=document.querySelectorAll(".checked");
+
+    elements.forEach(function(item){
+      
+        item.style.display="none";
+
+    });
+
+    
+}
+
+
+
+document.querySelector("#btnCreate").onclick = function () {
+
+    var item = document.querySelector("#txtItem").value
+
+    if (item == "") {
+        alert("lütefen bir değer giriniz..");
+        return;
+    }
+
+    CreateItem(item);
+};
+
+function CreateItem(item) {
+    var li = document.createElement("li");
+    var t = document.createTextNode(item);
+    li.className = "list-group-item";
+
+    li.appendChild(t);
+    list.appendChild(li);
+
+    var span = document.createElement("span");
+    var text = document.createTextNode("\u00D7");
+    span.className = "close";
+
+    span.appendChild(text);
+    li.appendChild(span);
+
+
+
+    span.onclick = function () {
+        var li = this.parentElement;
+        li.style.display = "none";
+        li.classList.remove("checked")
+    }
+}
+
+
+
+
